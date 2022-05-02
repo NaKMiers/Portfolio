489cc44dca0f5ea05125eac9eb9103de57a45c2a
AnK <56575187+NaKMiers@users.noreply.github.com>
2022-05-03 00:15:19 +0700

    custom cursor

--- js/cursor.js
+++ js/cursor.js
@@ -1,26 +1,130 @@
-import animejs from 'https://cdn.skypack.dev/animejs@3.2.1'
+var cursor = {
+   delay: 16,
+   _x: 0,
+   _y: 0,
+   endX: window.innerWidth / 2,
+   endY: window.innerHeight / 2,
+   cursorVisible: true,
+   cursorEnlarged: false,
+   $dot: document.querySelector('.cursor-dot'),
+   $outline: document.querySelector('.cursor-dot-outline'),
 
-function move({ clientX: x, clientY: y, target }) {
-   let scale = 1
+   init: function () {
+      // Set up element sizes
+      this.dotSize = this.$dot.offsetWidth
+      this.outlineSize = this.$outline.offsetWidth
 
-   if (target && target.classList && target.classList.contains('link')) {
-      scale = 1.75
-   }
+      this.setupEventListeners()
+      this.animateDotOutline()
+   },
 
-   const cursorElement = animejs({
-      targets: '.cursor',
-      translateX: Math.floor(x),
-      translateY: Math.floor(y),
-      rotate: x / -8 + 135,
-      scale,
-      easing: 'easeOutElastic(1, 1)',
-   })
+   updateCursor: function (e) {
+      var self = this
+
+      console.log(e)
+
+      // Show the cursor
+      self.cursorVisible = true
+      self.toggleCursorVisibility()
+
+      // Position the dot
+      self.endX = e.pageX
+      self.endY = e.pageY
+      self.$dot.style.top = self.endY + 'px'
+      self.$dot.style.left = self.endX + 'px'
+   },
+
+   setupEventListeners: function () {
+      var self = this
+
+      // Anchor hovering
+
+      let tags = ['a']
+      tags.forEach(selector => {
+         document.querySelectorAll(selector).forEach(function (el) {
+            el.addEventListener('mouseover', function () {
+               self.cursorEnlarged = true
+               self.toggleCursorSize()
+            })
+            el.addEventListener('mouseout', function () {
+               self.cursorEnlarged = false
+               self.toggleCursorSize()
+            })
+         })
+      })
+
+      // Click events
+      document.addEventListener('mousedown', function () {
+         self.cursorEnlarged = true
+         self.toggleCursorSize()
+      })
+      document.addEventListener('mouseup', function () {
+         self.cursorEnlarged = false
+         self.toggleCursorSize()
+      })
+
+      document.addEventListener('mousemove', function (e) {
+         // Show the cursor
+         self.cursorVisible = true
+         self.toggleCursorVisibility()
+
+         // Position the dot
+         self.endX = e.pageX
+         self.endY = e.pageY
+         self.$dot.style.top = self.endY + 'px'
+         self.$dot.style.left = self.endX + 'px'
+      })
+
+      // Hide/show cursor
+      document.addEventListener('mouseenter', function (e) {
+         self.cursorVisible = true
+         self.toggleCursorVisibility()
+         self.$dot.style.opacity = 1
+         self.$outline.style.opacity = 1
+      })
+
+      document.addEventListener('mouseleave', function (e) {
+         self.cursorVisible = true
+         self.toggleCursorVisibility()
+         self.$dot.style.opacity = 0
+         self.$outline.style.opacity = 0
+      })
+   },
+
+   animateDotOutline: function () {
+      var self = this
+
+      self._x += (self.endX - self._x) / self.delay
+      self._y += (self.endY - self._y) / self.delay
+      self.$outline.style.top = self._y + 'px'
+      self.$outline.style.left = self._x + 'px'
+
+      requestAnimationFrame(this.animateDotOutline.bind(self))
+   },
+
+   toggleCursorSize: function () {
+      var self = this
+
+      if (self.cursorEnlarged) {
+         self.$dot.style.transform = 'translate(-50%, -50%) scale(0.75)'
+         self.$outline.style.transform = 'translate(-50%, -50%) scale(1.5)'
+      } else {
+         self.$dot.style.transform = 'translate(-50%, -50%) scale(1)'
+         self.$outline.style.transform = 'translate(-50%, -50%) scale(1)'
+      }
+   },
+
+   toggleCursorVisibility: function () {
+      var self = this
+
+      if (self.cursorVisible) {
+         self.$dot.style.opacity = 1
+         self.$outline.style.opacity = 1
+      } else {
+         self.$dot.style.opacity = 0
+         self.$outline.style.opacity = 0
+      }
+   },
 }
 
-window.addEventListener('mousemove', move)
-
-window.onload = () =>
-   animejs({
-      targets: '.cursor',
-      opacity: 1,
-   })
+cursor.init()
